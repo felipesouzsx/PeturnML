630652dff4436580ef18a13b2c01460f5a3b5720
Felipe Souza <im.felipemontsouza@gmail.com>
2026-09-01 09:20:45 -0300

    add size constraints to CreatePostRequest

--- spring-api/src/main/java/com/felipesouza/peturn/post/CreatePostRequest.java
+++ spring-api/src/main/java/com/felipesouza/peturn/post/CreatePostRequest.java
@@ -3,12 +3,13 @@ package com.felipesouza.peturn.post;
 import com.felipesouza.peturn.pet.PetType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.web.multipart.MultipartFile;
 
 public record CreatePostRequest(
-   @NotBlank String petName,
+   @NotBlank @Size(max = 50) String petName,
    PetType petType,
-   @NotBlank String title,
-   @NotNull String description,
+   @NotBlank @Size(max = 100) String title,
+   @NotNull @Size(max = 500) String description,
    @NotNull MultipartFile petImage
 ) {}
